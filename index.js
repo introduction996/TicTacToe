2e852d552984d51da025d7ccfa03b49e2614dddc
johnny <ionutsbotnaru@gmail.com>
2023-11-02 10:30:03 +0200

    add board rendering

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+const cells = document.querySelectorAll('.cell');
+const playerOne = Player('Johnny', 'X');
+const playerTwo = Player('test player', 'O');
+let currentPlayer = 1;
+
 function Player(name, marker) {
     const score = 0;
     return {name, marker}
 }
 
 const gameBoard = (function(){
-    gameboard = ['X', 'O', 'O'];
+    gameboard = [];
+    // empty slots because i need a way to 
+    // tell between empty and not empty when 
+    //redering the board later on
+    for (let i = 0; i < 9; i++) {
+        gameboard[i] = 'empty'
+    }
     return gameboard
 })();
 
-const playerOne = Player('Johnny', 'X');
-const playerTwo = Player('test player', 'O');
-let currentPlayer = true;
-const gameControl = (function() {
-    const cells = document.querySelectorAll('.cell');
+const boardControl = (function() {
     function renderBoard() {
         for (let i = 0; i < gameboard.length; i++) {
-            console.log(i);
             cells.forEach(cell => {
-                if (i == cell.getAttribute('data-order')) {cell.innerText = gameboard[i]}
+                if (i == cell.getAttribute('data-order') && gameboard[i]!='empty') {cell.innerText = gameboard[i]}
             })
         }
     }
 
-    renderBoard()
+    return {renderBoard}
+})();
+
+const gameFlow = (function(){
+    cells.forEach(cell => cell.addEventListener('click', () => {
+        let i = cell.getAttribute('data-order');
+        gameboard[i] = currentPlayer == 1 ? playerOne.marker : playerTwo.marker
+        currentPlayer *= -1;
+        boardControl.renderBoard()
+    }))
 })();
