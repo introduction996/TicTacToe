111bacdc07dff5e6024b41e2e3107860f4454037
johnny <ionutsbotnaru@gmail.com>
2023-11-03 12:53:30 +0200

    add single-option cell functionality

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const cells = document.querySelectorAll('.cell');
 const playerOne = Player('Johnny', 'X');
 const playerTwo = Player('test player', 'O');
-let currentPlayer = 1;
+let currentPlayer = playerOne;
 
 function Player(name, marker) {
     const score = 0;
-    return {name, marker}
+    function increaseScore(){
+        score++
+    }
+    return {name, marker, increaseScore}
 }
 
 const gameBoard = (function(){
     gameboard = [];
     // empty slots because i need a way to 
     // tell between empty and not empty when 
-    //redering the board later on
+    // redering the board later on
     for (let i = 0; i < 9; i++) {
         gameboard[i] = 'empty'
     }
@@ -34,8 +37,11 @@ const boardControl = (function() {
 const gameFlow = (function(){
     cells.forEach(cell => cell.addEventListener('click', () => {
         let i = cell.getAttribute('data-order');
-        gameboard[i] = currentPlayer == 1 ? playerOne.marker : playerTwo.marker
-        currentPlayer *= -1;
-        boardControl.renderBoard()
+        if (gameboard[i]!= currentPlayer.marker) {
+            gameboard[i] = currentPlayer.marker == playerTwo.marker ? playerOne.marker : playerTwo.marker;
+            currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+            //saves function calls
+            boardControl.renderBoard()
+        }
     }))
 })();
